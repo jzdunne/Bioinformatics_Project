aa03d0f51d66b72bd50ca558271d5708dc274c4e
Joanne Dunne <j.z.dunne@dundee.ac.uk>
2015-02-24 18:10:24 +0000

    working cgi

--- public_html/cgi-bin/Querylink.py
+++ public_html/cgi-bin/Querylink.py
@@ -1,18 +1,31 @@
+#!/usr/bin/python
 import cgi
 import cgitb
 cgitb.enable()
 
 form = cgi.FieldStorage()
+import MySQLdb
+sql="SELECT Accession, Description FROM gene_info where Gene_id= %s"
+db=MySQLdb.connect(db='jzdunne', user='jzdunne', passwd='PxFPTOvk')
+cursor=db.cursor()
+cursor.execute(sql,form[''].value)
+result=cursor.fetchall()
+result
 
 print "Content-Type: text/html"     # HTML is following
 print                               # blank line, end of headers
 print "<html><head><TITLE>Sample Information</TITLE></head>"
 print "<body><H1>Sample Information</H1>"
-print "<table><tr><th>Gene ID</th><th>Gene Symbol</th><th>Gene title</th></tr>"
+print result
+print "<table><tr><th></th><th>Gene_ID</th><th>Gene Symbol</th><th>Gene title</th></tr>"
 
 for k in form.keys():
+  
+
         print "<tr><td>%s</td><td>%s</td></tr>"%(k, form[k].value)
 
 print "<table>"
 
+
 print "</body></html>"
+
