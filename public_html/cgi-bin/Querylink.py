3543a3535b3aeb8f786a56c80274e56ac0b85ca1
Joanne Dunne <j.z.dunne@dundee.ac.uk>
2015-02-27 00:00:27 +0000

    FINAL CGI

--- public_html/cgi-bin/Querylink.py
+++ public_html/cgi-bin/Querylink.py
@@ -5,7 +5,7 @@ cgitb.enable()
 
 form = cgi.FieldStorage()#stores information submitted to the form
 import MySQLdb #imports SQL database so it can be queried
-sql="SELECT Accession, Description FROM gene_info where Gene_id= %s" #SQL query
+sql="SELECT Gene_symbol, Gene_title FROM gene_info where Gene_id= %s" #SQL query
 db=MySQLdb.connect(db='jzdunne', user='jzdunne', passwd='PxFPTOvk') #establish SQL connection
 cursor=db.cursor() #establish cursor
 cursor.execute(sql,form[''].value) #cursor carries out query using form value ('' is the name of the form)
@@ -13,8 +13,6 @@ result=cursor.fetchall() #fetches results
 result
 #second and third query to establish results of experiment
 exprsql="SELECT sum(expression_value)/count(expression_value)FROM sample INNER JOIN expression ON sample.Sample_ID=expression.Sample_ID JOIN probes ON expression.ID_REF=probes.ID_REF WHERE Gene_id= %s AND subset=' CF'" 
-db=MySQLdb.connect(db='jzdunne', user='jzdunne', passwd='PxFPTOvk') #establish SQL connection
-cursor=db.cursor() #establish cursor
 cursor.execute(exprsql,form[''].value)
 exprresult=cursor.fetchall()
 exprresult
